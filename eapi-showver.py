8ae51e2a8e4203a1a2bcd659bb439d9f526ebccb
Fran Loehmann <fran.loehmann@gmail.com>
2019-08-09 10:09:28 -0700

    first commit

---
+++ eapi-showver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import pprint
+from jsonrpclib import Server
+
+switch = Server ("http://arista:arista@192.168.0.14/command-api")
+
+response = switch.runCmds( 1, ["show version"] )
+
+print
+print "The switch model name is " + response[0]["modelName"] + " and it is running " + response[0]["version"]
+print "\nHere is the json response:\n"
+print response
+print "\nHere is the pretty json response:\n"
+pprint.pprint(response)
+print 
+
---
+++ pyeapi-adduser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import pyeapi
+
+node = pyeapi.connect(host='192.168.0.14',username='arista',password='arista',return_node=True)
+
+users = node.api('users')
+
+users.create('testuser',secret='foo')
+users.set_privilege('testuser',value='15')
+users.set_role('testuser',value='network-admin')
+
